0bb13583f9d80f21a0982ebf06b25ee1d36450d4
Anczimaru <anczi101@gmail.com>
2018-08-07 14:46:47 +0200

    Created scrypt making TFRecords, rework data labeling to x1,y1,x2,y2

--- my_tools.py
+++ my_tools.py
@@ -1,37 +1,25 @@
-import functools
-import tensorflow
+import PIL
+import numpy as np
+import config
 import os
-import sys
+from matplotlib import pyplot as plt
 
-def doublewrap(function):
-    """
-    A decorator of decorator, allowing use of lazy property if no arguments are provided
-    """
-    @functools.wraps(function)
-    def decorator(*args, **kwargs):
-        if len(args) == 1 and len(kwargs) == 0 and callable(args[0]):
-            return function(args[0])
-        else:
-            return lambda wrapee: function(wrapee, *args, **kwargs)
-    return decorator
-
-@doublewrap
-def define_scope(function, scope = None, *args, **kwargs):
-    """
-    Lazy decorator, optimizes code by loading class Model parts only once to memory
-    Also its groups tf.Graph, in tensorboard into smaller, more readable parts
-    """    
-    attribute = '_cache_' + function.__name__
-    name = scope or function.__name__
+def show_image(n):
+    img, label = get_image_with_label(n)
+    print(label)
+    plt.imshow(img)
+    plt.show()
     
-    @property
-    @functools.wraps(function)
-    def decorator(self):
-        if not hasattr(self, attribute):
-            #Sorting Graph by Var_scope
-            with tf.variable_scope(name, *args, **kwargs):
-                setattr(self, attribute, function(self))
-                print("Initialized Model.{}".format(name))
-        return getattr(self, attribute)
     
-    return decorator
+    
+    
+def get_image_with_label(n):
+    """
+    Returns img with Image module and corresponding label
+    """
+    labels = np.load(config.LABEL_ORG)
+    label = labels[n]
+    img_name = str(n)+".png"
+    path_to_file = os.path.join(config.PIC_SRC_DIR,img_name)
+    img = PIL.Image.open(path_to_file)
+    return img, label
