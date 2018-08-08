3a8e0c4829a53951cf65ce9a5b47a5ff2742c3b3
Anczimaru <anczi101@gmail.com>
2018-08-08 16:04:08 +0200

    Pipeline finished, documentation started

--- my_tools.py
+++ my_tools.py
@@ -3,6 +3,8 @@ import numpy as np
 import config
 import os
 from matplotlib import pyplot as plt
+import functools
+import tensorflow as tf
 
 def show_image(n):
     """
@@ -50,3 +52,39 @@ def show_label_on_img(n):
     d.rectangle(coordinates,outline=(255,0,0,255))
     out = Image.alpha_composite(base, box)
     show_opened_image(out)
+    
+    
+    
+    
+def doublewrap(function):
+    """
+    A decorator of decorator, allowing use of lazy property if no arguments are provided
+    """
+    @functools.wraps(function)
+    def decorator(*args, **kwargs):
+        if len(args) == 1 and len(kwargs) == 0 and callable(args[0]):
+            return function(args[0])
+        else:
+            return lambda wrapee: function(wrapee, *args, **kwargs)
+    return decorator
+
+@doublewrap
+def define_scope(function, scope = None, *args, **kwargs):
+    """
+    Lazy decorator, optimizes code by loading class Model parts only once to memory
+    Also its groups tf.Graph, in tensorboard into smaller, more readable parts
+    """    
+    attribute = '_cache_' + function.__name__
+    name = scope or function.__name__
+    
+    @property
+    @functools.wraps(function)
+    def decorator(self):
+        if not hasattr(self, attribute):
+            #Sorting Graph by Var_scope
+            with tf.variable_scope(name, *args, **kwargs):
+                setattr(self, attribute, function(self))
+                print("Initialized Model.{}".format(name))
+        return getattr(self, attribute)
+    
+    return decorator
